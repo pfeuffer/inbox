b90f9bac5fe6e6922c9688b7415988aa622bc3b0
René Pfeuffer <rene@pfeuffer-web.de>
2017-11-15 17:15:39 +0100

    Add html frontend.

--- src/main/java/de/pfeufferweb/inbox/InboxController.java
+++ src/main/java/de/pfeufferweb/inbox/InboxController.java
@@ -1,13 +1,14 @@
 package de.pfeufferweb.inbox;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class InboxController {
     private final Inbox inbox;
@@ -17,13 +18,31 @@ public class InboxController {
         this.inbox = inbox;
     }
 
-    @GetMapping("/search")
-    public ResponseEntity<SearchResult> search(@RequestParam(name = "query", required = true) String query) {
-        return ResponseEntity.ok(inbox.search(query));
+    @GetMapping(value = "/search", produces = {"application/json", "application/xml"})
+    @ResponseBody
+    public SearchResult search(@RequestParam(name = "query", required = true) String query) {
+        return inbox.search(query);
     }
 
-    @GetMapping("/get")
-    public ResponseEntity<SearchResult> get(@RequestParam(name = "location", required = true) String location) {
-        return ResponseEntity.ok(inbox.get(location));
+    @GetMapping(value = "/search", produces = {"text/html"})
+    public String search(@RequestParam(name = "query", required = true) String query, Model model) {
+        SearchResult result = search(query);
+        model.addAttribute("items", result.getItems());
+        model.addAttribute("size", result.getItems().size());
+        return "get";
+    }
+
+    @GetMapping(value = "/get", produces = {"application/json", "application/xml"})
+    @ResponseBody
+    public SearchResult get(@RequestParam(name = "location", required = true) String location) {
+        return inbox.get(location);
+    }
+
+    @GetMapping(value = "/get", produces = {"text/html"})
+    public String get(@RequestParam(name = "location", required = true) String location, Model model) {
+        SearchResult result = get(location);
+        model.addAttribute("items", result.getItems());
+        model.addAttribute("size", result.getItems().size());
+        return "get";
     }
 }
